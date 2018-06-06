85d750755abd19d32dbda7e39742b08ad391a2bd
edgar <emolina@anzen.com.mx>
2018-06-06 17:05:59 -0500

    UPDATE: actualización descarga html crea folder

--- integration.py
+++ integration.py
@@ -1,20 +1,48 @@
 import json
 import urllib.request
+import time
+import os
 from pprint import pprint
 with open('prueba.json') as json_data:
     d = json.load(json_data)
-# print(d['urls'])
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+def files(url,):
+    t = x.split("/")
+    
+    #a_list = ["0","1","2"]
 
+    i = 0;
+    for folder in t:
+        if i != 0
+           cad = cad +"/"+ folder
+           i++
+        print (folder)        
+            
+    filename = t.pop()
+    return v[,filename]
+    
+    
 for x in d['urls']:
-	url = d['hostB']+x
-	fp = urllib.request.urlopen(url)
+	url2 = d['hostB']+x
+	fp = urllib.request.urlopen(url2)
 	mybytes = fp.read()
 	mystr = mybytes.decode("utf8")
 	fp.close()
-	tupla = x.partition("/") 
-	f = open(x,'w')
-	f.write(mystr)
-	f.close()
+	t = files(x);	
+	Cpath = "files/"+x
+	createFolder(Cpath)
+	ruta = ('files/'+x)
+	# print(ruta)
+	#f = open(ruta,'w+')
+	#time.sleep(2)
+	#f.write(mystr)
+	#f.close()
+	
 	
 # url = d['hostB']+d['urls'][0]
 # url2 = "https://www.banamex.com/firmaygana/index.html"
@@ -23,6 +51,6 @@ for x in d['urls']:
 # mystr = mybytes.decode("utf8")
 # fp.close()
 # print(mystr)
-# f = open('files/helloworld.js','w')
+# f = open('files/t1.html','w+')
 # f.write(mystr)
 # f.close()
